ddcb68d771d311cb1b253c0808c8b5ad10bd6e89
Alexandra Ionita <alexandraa_ionita@yahoo.com>
2020-03-05 16:35:39 +0000

    Ajax implemented for cards quantity and categories

--- app/controllers/wish_list_items_controller.rb
+++ app/controllers/wish_list_items_controller.rb
@@ -1,13 +1,18 @@
 class WishListItemsController < ApplicationController
+  respond_to :html, :js
+
+  respond_to do |format|
+    format.html
+    format.js
+  end
+
   def create
-    @item = Item.find(params[:wish_list_item][:item].to_i)
-    @wish_list_item = WishListItem.create(wish_list_item_params)
-    @wish_list_item.item = @item
+    @wish_list_item = WishListItem.new(wish_list_item_params)
     @wish_list_item.user = current_user
-    if !@wish_list_item.save
+    if @wish_list_item.save
+    else
       flash[:notice] = "Oh no, something went wrong!"
     end
-    redirect_to items_path
   end
 
   def index
@@ -44,6 +49,6 @@ class WishListItemsController < ApplicationController
   private
 
   def wish_list_item_params
-    params.require(:wish_list_item).permit(:amount)
+    params.require(:wish_list_item).permit(:amount, :item_id)
   end
 end
