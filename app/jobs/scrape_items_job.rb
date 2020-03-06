5b71c29c8b4d39935294598daa78b4496c9f0404
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-03-06 03:59:29 +0000

    Debugged Api & scraper

--- app/jobs/scrape_items_job.rb
+++ app/jobs/scrape_items_job.rb
@@ -20,47 +20,51 @@ class ScrapeItemsJob < ApplicationJob
       cards = html_doc.search('.fops-regular').children
       counter = 0
       index = 0
-      until counter == 3 do
-        card = cards[index]
-        name = card.search('h4.fop-title').children.first.text.strip
-        weight = card.search('.fop-catch-weight').text.strip
-        if card.search('.fop-price').text.strip[0] == "£"
-          price = card.search('.fop-price').text.strip[1..-1]
-        else
-          price = "0.#{card.search('.fop-price').text.strip[0..2]}".to_f
-          # binding.pry
+      begin
+        until counter == 3 do
+          card = cards[index]
+          name = card.search('h4.fop-title').children.first.text.strip
+          weight = card.search('.fop-catch-weight').text.strip
+          if card.search('.fop-price').text.strip[0] == "£"
+            price = card.search('.fop-price').text.strip[1..-1]
+          else
+            price = "0.#{card.search('.fop-price').text.strip[0..2]}".to_f
+            # binding.pry
+          end
+          quantity = weight.scan(/[\d|.]+/)
+          unit = weight.gsub(/[\d|.]+/, "")
+          emissions = (rand() * 10).round(2)
+          new_item = Item.new(
+          generic_name: item[:name.to_s],
+          generic_unit: item[:unit.to_s],
+          generic_quantity: item[:quantity.to_s],
+          category: item[:category.to_s],
+          sub_category: item[:sub_category.to_s],
+          image: item[:image.to_s],
+          seasonal: item[:seasonal.to_s],
+          name: name,
+          price: price,
+          quantity: quantity[0],
+          unit: unit,
+          retailer: 'Morrisons',
+          emission: emissions
+          )
+          if new_item.quantity.nil? || new_item.quantity == "0"
+            new_item.quantity = "1"
+          end
+          unless new_item.name.include? item[:name.to_s].split(" ")[0].capitalize
+            new_item.name = nil
+          end
+          if new_item.unit.nil?
+            new_item.unit = generic_unit
+          end
+          if new_item.save
+            counter += 1
+          end
+          index += 1
         end
-        quantity = weight.scan(/[\d|.]+/)
-        unit = weight.gsub(/[\d|.]+/, "")
-        emissions = (rand() * 10).round(2)
-        new_item = Item.new(
-        generic_name: item[:name.to_s],
-        generic_unit: item[:unit.to_s],
-        generic_quantity: item[:quantity.to_s],
-        category: item[:category.to_s],
-        sub_category: item[:sub_category.to_s],
-        image: item[:image.to_s],
-        seasonal: item[:seasonal.to_s],
-        name: name,
-        price: price,
-        quantity: quantity[0],
-        unit: unit,
-        retailer: 'Morrisons',
-        emission: emissions
-        )
-        if new_item.quantity.nil? || new_item.quantity == "0"
-          new_item.quantity = "1"
-        end
-        unless new_item.name.include? item[:name.to_s].split(" ")[0].capitalize
-          new_item.name = nil
-        end
-        if new_item.unit.nil?
-          new_item.unit = generic_unit
-        end
-        if new_item.save
-          counter += 1
-        end
-        index += 1
+      rescue
+        next
       end
     end
 # Do something later
