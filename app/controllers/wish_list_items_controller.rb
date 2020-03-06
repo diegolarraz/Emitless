4d316d5ab1e8dc1d4203ba90910d8a8cd960ba2e
Anne de Joly <anne.dejoly@edhec.com>
2020-03-06 15:02:21 +0000

    rescue removed

--- app/controllers/wish_list_items_controller.rb
+++ app/controllers/wish_list_items_controller.rb
@@ -35,23 +35,18 @@ class WishListItemsController < ApplicationController
 
       @wish_list_items.each do |wish_list_item|
         # Finding the best item with the lowest emissions
-        begin
-          best_item = Item.order(emission: :asc).where("generic_name = ? AND retailer = ?", wish_list_item.item.generic_name.to_s, retailer.to_s).first
-          # Quantity of item
-          # @basket[retailer.downcase.to_sym][:items][best_item] = best_item
-          @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name] = [1, best_item]
+        best_item = Item.order(emission: :asc).where("generic_name = ? AND retailer = ?", wish_list_item.item.generic_name.to_s, retailer.to_s).first
+        # Quantity of item
+        # @basket[retailer.downcase.to_sym][:items][best_item] = best_item
+        @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name] = [1, best_item]
 
-
-          while @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0] * best_item.quantity.to_i < wish_list_item.amount * best_item.generic_quantity
-            @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0] += 1
-          end
-
-          # Total price and emissions
-          @basket[retailer.downcase.to_sym][:price] += (best_item.price * @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0]) unless best_item.nil?
-          @basket[retailer.downcase.to_sym][:emissions] += (best_item.emission * @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0]) unless best_item.nil?
-        rescue
-          next
+        while @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0] * best_item.quantity.to_i < wish_list_item.amount * best_item.generic_quantity
+          @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0] += 1
         end
+
+        # Total price and emissions
+        @basket[retailer.downcase.to_sym][:price] += (best_item.price * @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0]) unless best_item.nil?
+        @basket[retailer.downcase.to_sym][:emissions] += (best_item.emission * @basket[retailer.downcase.to_sym][:items][wish_list_item.item.generic_name][0]) unless best_item.nil?
       end
     end
 
