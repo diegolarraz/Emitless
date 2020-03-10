20802061a761ee61ec19787a5c7f873143e03475
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-03-10 12:55:25 +0000

    bugfix swaps

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,24 +1,23 @@
 class BasketsController < ApplicationController
   def show
-      unless params["format"] == "pdf"
-        if params[:id]
-          @final_basket = Basket.find(params[:id])
-        end
-        @final_basket = Basket.new(retailer: params[:retailer], user: current_user)
-        @final_basket.emissions = params[:basket][:emissions].to_f
-        @final_basket.price = params[:basket][:price].to_f
+    if params["format"] == "pdf"
+      @final_basket = Basket.find(params["basket"])
+    elsif params[:id]
+      @final_basket = Basket.find(params[:id].to_i)
+    else
+      @final_basket = Basket.new(retailer: params[:retailer], user: current_user)
+      @final_basket.emissions = params[:basket][:emissions].to_f
+      @final_basket.price = params[:basket][:price].to_f
 
-        @final_basket.save
+      @final_basket.save
 
-        items = params[:basket][:items]
+      items = params[:basket][:items]
 
-        items.each do |generic_name, infos|
-          item = BasketItem.create(item: Item.find(infos[1].to_i), basket: @final_basket, amount: infos[0].to_i)
-        end
-      else
-        # {"basket"=>"24", "retailer"=>"tesco", "format"=>"pdf"}
-        @final_basket = Basket.find(params["basket"])
+      items.each do |generic_name, infos|
+        item = BasketItem.create(item: Item.find(infos[1].to_i), basket: @final_basket, amount: infos[0].to_i)
       end
+    end
+        # {"basket"=>"24", "retailer"=>"tesco", "format"=>"pdf"
 
 
     respond_to do |format|
